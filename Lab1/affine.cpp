a524f5a120efd3f74a24ea8eef8a4895e683d431
Sanjay <118333997+Sanjay-109@users.noreply.github.com>
2024-08-14 11:09:54 +0530

    Update affine.cpp

--- Lab1/affine.cpp
+++ Lab1/affine.cpp
@@ -1,27 +1,55 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 const int a = 17;
 const int b = 20;
+const int MOD = 26;
 
-string encryptMessage(string msg) {
-	string cipher = ""; 
-	for (int i = 0; i < msg.length(); i++) {
-		
-		if(msg[i]!=' ') 
-			cipher = cipher + (char) ((((a * (islower(msg[i]) ? msg[i] - 'a' : msg[i] - 'A') ) + b) % 26) + 'A');
-		else
-			cipher += msg[i];	 
-	}
-	return cipher;
+int modInverse(int a, int mod) {
+    a = a % mod;
+    for (int x = 1; x < mod; x++) {
+        if ((a * x) % mod == 1)
+            return x;
+    }
+    return 1;
 }
 
+string encryptMessage(string msg) {
+    string cipher = ""; 
+    for (int i = 0; i < msg.length(); i++) {
+        if (isalpha(msg[i])) {
+            char base = islower(msg[i]) ? 'a' : 'A';
+            cipher += char((a * (msg[i] - base) + b) % MOD + base);
+        } else {
+            cipher += msg[i];
+        }
+    }
+    return cipher;
+}
+
+string decryptMessage(string cipher) {
+    string message = "";
+    int a_inv = modInverse(a, MOD); 
+
+    for (int i = 0; i < cipher.length(); i++) {
+        if (isalpha(cipher[i])) {
+            char base = islower(cipher[i]) ? 'a' : 'A';
+            message += char((a_inv * ((cipher[i] - base - b + MOD) % MOD)) % MOD + base);
+        } else {
+            message += cipher[i]; 
+        }
+    }
+    return message;
+}
 
 int main(void) {
-	string msg = "Hello World";
-	
-	string cipherText = encryptMessage(msg);
-	cout << "Encrypted Message is : " << cipherText<<endl;
+    string msg = "Hello World";
+    
+    string cipherText = encryptMessage(msg);
+    cout << "Encrypted Message is : " << cipherText << endl;
 
-	return 0;
+    string decryptedText = decryptMessage(cipherText);
+    cout << "Decrypted Message is : " << decryptedText << endl;
+
+    return 0;
 }
